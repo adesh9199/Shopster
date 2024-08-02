0199994bd3ac8fbb85c86bb1c6b78e54fb3495b7
Adesh Kumar Dubey <adeshkumardubey889@gmail.com>
2024-08-02 16:45:39 +0530

    env

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const dbgr = require("debug")("Development:mongoose");
+// const mongoose = require('mongoose');
+// const config = require('config');
+// const dbgr = require("debug")("Development:mongoose");
 
-mongoose.connect(`${config.get("MONGODB_URI")}/Shopster`)
-    .then(() => {
-        dbgr("Connected")
-    })
-    .catch((e) => {
-        dbgr(e)
-    })
+// mongoose.connect(`${config.get("MONGODB_URI")}/Shopster`)
+//     .then(() => {
+//         dbgr("Connected")
+//     })
+//     .catch((e) => {
+//         dbgr(e)
+//     })
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -3,9 +3,9 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
-
+const URL = process.env.MONGODB_URI
 mongoose
-  .connect(`${config.get("MONGODB_URI")}/Shopster`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     dbgr("connected");
   })
